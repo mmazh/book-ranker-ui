641256b7bb18fa32a6dac45e4eab3a317cb2bac3
madeeha <m4deeh4@gmail.com>
2023-07-06 19:29:43 -0400

    Browse tab component view generation and CSS tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { BrowseComponent } from './pages/browse/browse.component';
 
 const routes: Routes = [
-  { path: '', component: LeaderboardComponent }
+  { path: '', component: LeaderboardComponent },
+  { path: 'browse', component: BrowseComponent }
 ];
 
 @NgModule({
--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/book.service';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.css']
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit {
+
+  private books: any;
+  private votes: any;
+
+  public booksLoaded: boolean = false;
+  public votesLoaded: boolean = false;
+  public votesDict: {[bookId: number]: { username: string, score: number}[] } = {};
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.bookService.getAllBooks().subscribe((response: any) => {
+      this.books = response;
+      this.booksLoaded = true;
+    });
+
+    this.bookService.getAllVotes().subscribe((response: any) => {
+      this.votes = response;
+      this.votesLoaded = true;
+    });
+  }
+
+  public getArrayOfBookIds() {
+    this.refreshVotesDict();
+    let keys: number[] = [];
+    for (var book of this.books) {
+      if (this.votesDict[book._id] == undefined) {
+        this.votesDict[book._id] = [];
+      }
+      keys.push(book._id);
+    }
+    return keys;
+  }
+
+  private refreshVotesDict() {
+    this.votesDict = {};
+    for (var singleVote of this.votes) {
+      if (this.votesDict[singleVote._bookId] == undefined) {
+        this.votesDict[singleVote._bookId] = [];
+      }
+      this.votesDict[singleVote._bookId].push({username: singleVote.user, score: singleVote.vote});
+    }
+  }
+
+
+  public getTitleAuthorString(bookId: number) {
+    for (var book of this.books) {
+      if (book._id == bookId) {
+        return `${book.title}, ${book.author}`;
+      }
+    }
+    return ""
+  }
+
 
 }
