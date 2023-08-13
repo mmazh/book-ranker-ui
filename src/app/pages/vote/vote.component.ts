2afd0bca6b391f58ed32b0f77df9b261660a3971
madeeha <m4deeh4@gmail.com>
2023-08-13 16:23:50 -0400

    Added authentication to vote page

--- src/app/pages/vote/vote.component.ts
+++ src/app/pages/vote/vote.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { BookService } from 'src/app/book.service';
+import { AuthService } from 'src/app/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vote',
@@ -10,90 +12,63 @@ import { BookService } from 'src/app/book.service';
 export class VoteComponent implements OnInit {
 
   public books: any;
+  public loaded: boolean = false;
   private votes: any;
-  private users: any;
+  private userId: number = -1;
 
   public voteForm = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(1),
-      Validators.pattern('^[-a-zA-Z0-9]+$')]),
-    book: new FormControl('', [
-      Validators.required,
-      Validators.minLength(1)]),
-    stars: new FormControl(0, [
-      Validators.required,
-      Validators.min(1)])
+    book: new FormControl('', [Validators.required]),
+    stars: new FormControl(0, [Validators.required, Validators.min(1)])
   });
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    if (this.authService.tokenExpired()) this.router.navigate(['login']);
+    const tokenPayload = this.authService.tokenPayload();
+    this.userId = tokenPayload['userid'];
     this.bookService.getAllBooks().subscribe((response: any) => {
       this.books = response;
-    });
-    this.bookService.getAllVotes().subscribe((response: any) => {
-      this.votes = response;
-    });
-    this.bookService.getAllUsers().subscribe((response: any) => {
-      this.users = response;
+      this.bookService.getAllVotesForUser(this.userId).subscribe((response: any) => {
+        this.votes = response;
+        this.loaded = true;
+      });
     });
   }
 
   public onSubmit(): void {
     if (this.voteForm.invalid) return;
     let payload = this.createPayload();
-    if (typeof payload.userId === "undefined") {
-      window.alert("User does not exist");
-      window.location.reload();
+    let voteId = this.findExistingVoteId(payload);
+    if (voteId) {
+      this.updateVote(payload, voteId);
       return;
     }
-    let voteId = this.findExistingVoteId(payload);
-    if (typeof voteId !== "undefined") this.updateVote(payload, voteId);
-    else
-    {
-      this.bookService.createNewVote(payload).subscribe((response: any) => {
-        console.log(response);
-        window.location.reload();
-      });
-    }
+    this.bookService.createNewVote(payload).subscribe((response: any) => {
+      console.log(response);
+      window.location.reload();
+    });
   }
 
   private updateVote(payload: Object, voteId: number) {
     if (window.confirm("You already voted on this book. Update existing vote?")) {
       this.bookService.updateVote(voteId, payload).subscribe((response: any) => {
-        console.log(response);
+        window.location.reload();
       });
     }
-    window.location.reload();
   }
 
   private createPayload() {
     let bookId = this.findBookId(this.voteForm.value.book);
-    let userId = this.findUserId(this.voteForm.value.username);
-    return {
-      bookId: bookId,
-      userId: userId,
-      stars: this.voteForm.value.stars
-    };
+    return { bookId: bookId, userId: this.userId, stars: this.voteForm.value.stars };
   }
 
-  private findExistingVoteId(payload: {bookId: string, userId: string}) {
+  private findExistingVoteId(payload: {bookId: string, userId: number}) {
     return this.votes.find((x: any) => x.userId === payload.userId && x.bookId === payload.bookId)?.voteId;
-  }
-
-  private findUserId(username: string | null | undefined) {
-    return this.users.find((x: any) => x.username === username)?.userId;
   }
 
   private findBookId(titleAuthor: string | null | undefined) {
     return this.books.find((x: any) => `${x.title}, ${x.author}` === titleAuthor)?.bookId;
   }
-
-  get username() { return this.voteForm.get('username'); }
-
-  get book() { return this.voteForm.get('book'); }
-
-  get stars() { return this.voteForm.get('stars'); }
 
 }
